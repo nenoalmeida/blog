742e22deda7c1a8bc207a99b46e3165731694902
Neno Almeida <nenoxvx@icloud.com>
2016-09-24 13:38:50 +0000

    Adicionando os callbacks no routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root to: "home#index"
   # You can have the root of your site routed with "root"
